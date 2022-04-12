6582d638b0874cc52bf24b8a5af4072c9e72eba4
Giuseppe Pascale <giuseppe.pascale03@gmail.com>
2022-04-13 00:21:17 +0200

    :sparkles: Adding support for forward geocoding

---
+++ pages/api/geocode.js
@@ -0,0 +1,11 @@
+import { queryGeocode } from 'lib/ps';
+
+export default async function handler(req, res) {
+  const { query } = req.query;
+  if (!query) return res.status(400).end('Bad Request.');
+
+  const data = await queryGeocode(query);
+
+  if (data) return res.status(200).send(data);
+  else return res.status(500).end('Internal Server Error.');
+}
